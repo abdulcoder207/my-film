9f4330660096eb02bc05d2385a6e3726bd07b680
abdulcoder207 <abdulmuizridwan158@gmail.com>
2024-09-08 13:29:14 +0700

    testing

---
+++ fot.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("./footer/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("content-placeholder").innerHTML = data;
+        });
+});
---
+++ list/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("./nav.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../footer/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("content-placeholder").innerHTML = data;
+        });
+});
+// fetch('/list/footer.html')
+// .then(response => response.text())
+// .then(data => {
+//     // Menyisipkan konten ke dalam elemen
+//     document.getElementById('content-placeholder').innerHTML = data;
+
+//     // Mengekstrak dan mengeksekusi skrip dari konten yang dimuat
+//     const scripts = document.getElementById('content-placeholder').getElementsByTagName('script');
+//     for (let script of scripts) {
+//         eval(script.innerText);
+//     }
+// })
+// .catch(error => console.error('Error:', error));
